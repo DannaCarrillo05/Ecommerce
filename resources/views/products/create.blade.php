2570a2d1653bf37a219a2d7c467c140f558513e7
DannaCarrillo05 <dcarrillo882@unab.edu.co>
2025-09-11 12:44:38 -0500

    layouts basicos

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
+@section('content')
     <h1>FORMULARIO DE PRODUCTOS</h1>
     <form action="" method="POST" enctype="multipart/form-data">
         <div>
@@ -29,6 +21,4 @@
         </div>
         <button type="submit">Guardar</button>
     </form>
-</body>
-
-</html>
+@endsection
